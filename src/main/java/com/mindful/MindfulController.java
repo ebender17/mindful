82953ac29852291ebffd7e9278e784b80076abf9
Administrator <Administrator@DESKTOP-099UQRG>
2020-09-26 16:30:52 -0400

    Add Javadoc and added default / endpoint

--- src/main/java/com/mindful/MindfulController.java
+++ src/main/java/com/mindful/MindfulController.java
@@ -6,7 +6,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MindfulController {
 	
+	/*
+	 * Handle /start endpoint
+	 *@return  
+	 *
+	 **/
 	@RequestMapping("/index")
+	public String index() { 
+		return "index";
+	}
+	
+	@RequestMapping("/")
 	public String start() { 
 		return "index";
 	}
